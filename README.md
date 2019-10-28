78fda58e79d98fb55b04b0f441e31ecf02bb5a6e
Yosuke Saito <yosuke.saito1202@gmail.com>
2019-10-28 16:41:53 +0900

    add input.txt

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 ```sh
 $ docker-compose run py sh
 $ echo 'input' | python main.py # in container
+$ cat input.txt | python main.py # multiple lines input
 ```
--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-N = int(input())
+N, K = map(lambda i: int(i), input().split(' '))
+R = list(map(lambda i: int(i), input().split(' ')))
 
 def main():
-    cards = ['1', '2', '3', '4', '5', '6']
-    for i in range(0, N % 30):
-        cards = swap(cards, i)
-    print(''.join(cards))
-
-def swap(cards, i):
-    n = i % 5
-    m = i % 5 + 1
-    tmp = cards[n]
-    cards[n] = cards[m]
-    cards[m] = tmp
-    return cards 
+    rate = 0
+    R.sort()
+    for i in range(0, K):
+        rate = (rate + R[i + N - K]) / 2
+    print(rate)
 
 main()
