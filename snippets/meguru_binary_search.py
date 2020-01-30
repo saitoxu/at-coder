7d128df9b19b52819490d3484ecc71ce88d7eaac
Yosuke Saito <yosuke.saito1202@gmail.com>
2020-01-30 09:36:49 +0900

    add snippet

---
+++ snippets/meguru_binary_search.py
@@ -0,0 +1,20 @@
+# めぐる式二分探索
+# ref. https://qiita.com/drken/items/97e37dd6143e33a64c8c
+
+
+def is_ok(A, index, key):
+    return A[index] >= key
+
+
+def binary_search(A, key):
+    ng = -1
+    ok = len(A)
+
+    while abs(ok - ng) > 1:
+        mid = (ok + ng) // 2
+        if is_ok(A, mid, key):
+            ok = mid
+        else:
+            ng = mid
+
+    return ok
