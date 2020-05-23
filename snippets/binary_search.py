124e478f95da9e15500bb408a9082244c07d1e0b
Yosuke Saito <yosuke.saito1202@gmail.com>
2020-05-23 15:19:24 +0900

    tweak

--- snippets/binary_search.py
+++ snippets/binary_search.py
@@ -3,15 +3,15 @@
 
 
 def binary_search(arr, l, r, x):
-    if r >= l: 
+    if r >= l:
         mid = l + (r - l) // 2
-        if arr[mid] == x: 
-            return mid 
-        elif arr[mid] > x: 
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x:
             return binary_search(arr, l, mid - 1, x)
-        else: 
+        else:
             return binary_search(arr, mid + 1, r, x)
-    else: 
+    else:
         return -1
 
 
